823f1f11e7580e8d39cbb28f79b037002dedd29e
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2023-05-19 16:55:39 +0100

    First upload

---
+++ src/test/java/com/mustaphaslimani/infractionservice/InfractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustaphaslimani.infractionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
